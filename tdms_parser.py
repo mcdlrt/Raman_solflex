efca1742ff9d045a4b4370b513a9526dc5f845b1
mcdlrt <47090482+mcdlrt@users.noreply.github.com>
2019-05-29 14:17:58 +0200

    Update tdms_parser.py

--- tdms_parser.py
+++ tdms_parser.py
@@ -7,7 +7,7 @@ Created on Wed May 29 11:55:36 2019
 
 import os
 
-class tdms_file(filename,timearray = None):
+class tdms_file:
     def __init__(self,filename):
         self.filename = filename
-        self.ctime = os.path.getctime(self.filename)
+        self.ctime = os.path.getctime(self.filename)        # creation time since epoch
