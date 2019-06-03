8116cd42a9a57262cee0ae72cd9dd9df9ede97c7
nikita-nikitskiy <50732597+nikita-nikitskiy@users.noreply.github.com>
2019-06-03 12:06:02 +0200

    Update tdms_parser.py

--- tdms_parser.py
+++ tdms_parser.py
@@ -6,8 +6,14 @@ Created on Wed May 29 11:55:36 2019
 """
 
 import os
+import npTDMS
+from nptdms import TdmsFile
+import matplotlib.pyplot as plt
+import scipy as sp
+import numpy as np
 
 class tdms_file(filename,timearray = None):
     def __init__(self,filename):
         self.filename = filename
         self.ctime = os.path.getctime(self.filename)
+        
