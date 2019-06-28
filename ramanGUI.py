cecdca5578ac744b4427a56bc2d91e0e5f75bce5
nikita-nikitskiy <50732597+nikita-nikitskiy@users.noreply.github.com>
2019-06-28 14:37:43 +0200

    updated GUI

--- ramanGUI.py
+++ ramanGUI.py
@@ -10,84 +10,115 @@ import tdms_parser as tdp
 from PyQt5.QtWidgets import QLabel, QComboBox, QLineEdit, QTextEdit, QGridLayout, QMainWindow, QAction, qApp, QFileDialog, QApplication, QWidget, QPushButton, QToolTip, QMessageBox, QDesktopWidget
 from PyQt5.QtGui import QIcon
 import PyQt5.QtCore
+import raman_parser as rp
 
 class RamanGUI(QMainWindow):    
+    '''
+    This class creates gui application for raman+tensile stage measurements
+    Atrebutes:
+        
+    '''
+
     def __init__(self): 
         super().__init__()
         self.initUI()
         
     def initUI(self):
         
-        '''
-        '''
-        
         self.homedir = 'C:/'       
         
+        #An exit action garanties you sucsesseful closing of the window(see def closeEvent)
         exitAction = QAction(QIcon('test.png'), '&Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit Application')
         exitAction.triggered.connect(self.closeEvent)
         
+        #A status bar in the left bottom corner of the window
         self.statusBar()
         self.statusBar().showMessage('Ready')
         
+        #A button opens a dialog window to find a .tdms file(see def openTDMS)
         tdmsedit = QPushButton('Open tdms', self)
         tdmsedit.clicked.connect(self.openTDMS)
         tdmsedit.move(50, 60)
         
+        #A line shows your .tdms file name 
         self.tdmsline = QLineEdit(self)
         self.tdmsline.setReadOnly(True)
         self.tdmsline.setText('...')
         self.tdmsline.move(170, 60)
         
+        #A button opens a dialog window to find a .txt file with Raman data(see def openRaman)
         ramanbtn = QPushButton('Open raman', self)
         ramanbtn.clicked.connect(self.openRaman)
         ramanbtn.move(50, 100)
         
+        #A line shows your .txt file name 
         self.ramanline = QLineEdit(self)
         self.ramanline.setReadOnly(True)
         self.ramanline.setText('...')
         self.ramanline.move(170, 100)
         
+        
         startbtn = QPushButton('Start!', self)
         startbtn.clicked.connect(self.start)
         startbtn.move(550, 450)
         
+        #A tool where u can chose a crystal orientation
         cryor = QComboBox(self)
         cryor.addItems(['110', '100'])
         cryor.move(50, 140)
         cryor.activated[str].connect(self.crystalorientationset)
         
-        dirbtn  = QPushButton('Set directory', self)
-        dirbtn.clicked.connect(self.setHomeDir)
-        dirbtn.move(50, 180)
-        
+        #Group of line editers where you can see the value of length/width/thickness
+        #from your .tdms file or set it manualy 
         self.lenlab = QLabel(self)
         self.lenlab.setText('Length, mm')
-        self.lenlab.move(50, 220)
+        self.lenlab.move(450, 60)
         
         self.lenline = QLineEdit(self)
         self.lenline.setText('...')
         self.lenline.textChanged[str].connect(self.setLength)
-        self.lenline.move(150, 220)
+        self.lenline.move(570, 60)
         
         self.widlab = QLabel(self)
         self.widlab.setText('Width, mm')
-        self.widlab.move(50, 260)
+        self.widlab.move(450, 100)
         
         self.widline = QLineEdit(self)
         self.widline.setText('...')
         self.lenline.textChanged[str].connect(self.setWidth)
-        self.widline.move(150, 260)
+        self.widline.move(570, 100)
         
         self.thicklab = QLabel(self)
         self.thicklab.setText('Thickness, mm')
-        self.thicklab.move(50, 300)
+        self.thicklab.move(450, 140)
         
         self.thickline = QLineEdit(self)
         self.thickline.setText('...')
         self.lenline.textChanged[str].connect(self.setThick)
-        self.thickline.move(150, 300)
+        self.thickline.move(570, 140)
+        
+        #A button creates a dialog window when you can set up your home directory
+        dirbtn  = QPushButton('Set directory', self)
+        dirbtn.clicked.connect(self.setHomeDir)
+        dirbtn.move(50, 180)
+        
+        xcombo = QComboBox(self)
+        xcombo.addItems(['Time', 'Force', 'Elongation', 'Stress'])
+        xcombo.move(200, 220)
+        self.x = 'Time'
+        xcombo.activated[str].connect(self.setX)
+        
+        ycombo = QComboBox(self)
+        ycombo.addItems(['Time', 'Force', 'Elongation', 'Stress'])
+        ycombo.move(350, 220)
+        self.y = 'Time'
+        ycombo.activated[str].connect(self.setY)
+        
+        plot = QPushButton('Plot it!', self)
+        #plot.clicked.connect(self.plotgraph(self.x, self.y))
+        plot.move(500, 220)
         
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&Menu')
@@ -117,7 +148,7 @@ class RamanGUI(QMainWindow):
         self.tdms_name = QFileDialog(self).getOpenFileName(self, "Tensile stage data", self.homedir, "TDMS files (*.tdms)")
         self.tdmsline.setText(self.tdms_name[0])
         self.tdms_file = tdp.tdms(self.tdms_name[0])
-        self.tdms_file.parameters()
+        
         self.thickline.setText(str(self.tdms_file.Thickness))
         self.widline.setText(str(self.tdms_file.Width))
         self.lenline.setText(str(self.tdms_file.Length))
@@ -130,14 +161,25 @@ class RamanGUI(QMainWindow):
     
     def setLength(self, length):
         self.tdms_file.Length = float(length)
+    
+    def plotgraph(self, x, y):
+        try:
+            self.tdms_file.plot(x, y)
+        except:
+            print("You didn't choose the file!")
         
     def openRaman(self, event):
-        self.raman_name = QFileDialog(self).getOpenFileName(self, "Raman data", self.homedir, "TXT files (*.txt)")
+        self.raman_name = QFileDialog(self).getOpenFileNames(self, "Raman data", self.homedir, "TXT files (*.txt)")
         self.ramanline.setText(self.raman_name[0])
        
     def crystalorientationset(self, text):
         self.crystalorientation = text
-        print(self.crystalorientation)
+   
+    def setX(self, text):
+        self.x = text
+        
+    def setY(self, text):
+        self.y = text
     
     def start(self, event):
         print('START!')
